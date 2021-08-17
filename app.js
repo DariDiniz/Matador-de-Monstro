8847185811bf8be9b99aca243f19385bff660da6
DariDiniz <darianydiniz@gmail.com>
2021-08-17 15:56:08 -0300

    Add ataque no jogo

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+        },
+        attack(especial){
+
+        },
+        hurt(min, max, especial){
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max +plus)
+            this.playerLife = Math.max(this.playerLife - hurt, 0)
+
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
         }
 
     },
